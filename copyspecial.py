7a3112a6c5183f2639da0d36e04b600332f8e994
Ruben Espino <56930001+r-o-e-574@users.noreply.github.com>
2020-06-11 09:30:30 -0500

    init commit

--- copyspecial.py
+++ copyspecial.py
@@ -7,7 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # give credits
-__author__ = "???"
+__author__ = "Ruben Espino got help from Chris Warren"
 
 import re
 import os
@@ -19,17 +19,41 @@ import argparse
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
-    # your code here
-    return
+    path_list = [
+        os.path.abspath(os.path.join(dirname, files))
+        for files in (os.listdir(dirname))
+        if re.search(r'__(\w+)__', files)
+    ]
+    return path_list
 
 
 def copy_to(path_list, dest_dir):
-    # your code here
+    """Copy files to dest folder"""
+    try:
+        os.makedirs(dest_dir)
+    except OSError as error:
+        print(error)
+        exit(1)
+    for path in path_list:
+        file_name = os.path.basename(path)
+        current_path = os.path.dirname(path)
+        absolute_path = os.path.join(current_path, dest_dir, file_name)
+        shutil.copyfile(path, absolute_path)
     return
 
 
 def zip_to(path_list, dest_zip):
-    # your code here
+    """Zips the files in path list to dest zip file """
+    file_list = ''
+    for path in path_list:
+        file_list += path + ' '
+    print("Command I'm going to do:")
+    print('zip -j', dest_zip, file_list)
+    try:
+        subprocess.call(['zip', '-j', dest_zip] + path_list)
+    except OSError as error:
+        print(error)
+        exit(1)
     return
 
 
@@ -40,6 +64,7 @@ def main(args):
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
     # TODO: add one more argument definition to parse the 'from_dir' argument
+    parser.add_argument('from_dir', help='dir to search for files')
     ns = parser.parse_args(args)
 
     # TODO: you must write your own code to get the command line args.
@@ -51,6 +76,16 @@ def main(args):
     # exit(1).
 
     # Your code here: Invoke (call) your functions
+    if not ns:
+        parser.print_usage()
+        sys.exit(1)
+    special_paths = get_special_paths(ns.from_dir)
+    if ns.todir:
+        copy_to(special_paths, ns.todir)
+    if ns.tozip:
+        zip_to(special_paths, ns.tozip)
+    if not ns.todir and not ns.tozip:
+        print("\n".join(special_paths))
 
 
 if __name__ == "__main__":
